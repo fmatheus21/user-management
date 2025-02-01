8abeaeb9bddd745e0bb8889b3ec119af409e3f09
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-02-01 20:07:15 -0300

    Criação de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,10 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./pages/user/user.module').then((m) => m.UserModule),
+  },
   //{ path: '', redirectTo: '/auth', pathMatch: 'full' },
 ];
