8c29e940aa89daed3f2d04e4d8155b078d9c3420
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-02-01 19:47:10 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./pages/auth/auth.module').then((m) => m.AuthModule),
+  },
+  //{ path: '', redirectTo: '/auth', pathMatch: 'full' },
+];
