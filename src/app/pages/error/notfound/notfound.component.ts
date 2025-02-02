7af6ab54c6d7447831fdb625e0f46e0f35001036
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-02-01 23:01:04 -0300

    Implementando página de erro

--- src/app/pages/error/notfound/notfound.component.ts
+++ src/app/pages/error/notfound/notfound.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './notfound.component.html',
-  styleUrl: './notfound.component.css'
+  styleUrl: './notfound.component.css',
 })
-export class NotfoundComponent {
-
-}
+export class NotfoundComponent {}
